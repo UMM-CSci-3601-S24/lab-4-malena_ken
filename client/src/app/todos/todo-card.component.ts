996407720c8d17cad93d4d04d00b9a2fd2792073
Ken Broden <brod0268@morris.umn.edu>
2024-02-13 13:38:45 -0600

    Created todo-card and todo.service files
    Co-authored-by: Malena Mahoney <mahon479@morris.umn.edu>

--- client/src/app/todos/todo-card.component.ts
+++ client/src/app/todos/todo-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from './todo';
+import { RouterLink } from '@angular/router';
+import { MatButton } from '@angular/material/button';
+import { MatCard, MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle, MatCardContent, MatCardActions } from '@angular/material/card';
 
 @Component({
   selector: 'app-todo-card',
-  standalone: true,
-  imports: [],
   templateUrl: './todo-card.component.html',
-  styleUrl: './todo-card.component.scss'
+  styleUrls: ['./todo-card.component.scss'],
+  standalone: true,
+  imports: [MatCard, MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle, MatCardContent, MatCardActions, MatButton, RouterLink]
 })
 export class TodoCardComponent {
-
+  @Input() todo: Todo;
+  @Input() simple ? = false;
 }
--- client/src/app/todos/todo-profile.component.ts
+++ client/src/app/todos/todo-profile.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+//import { NgIf } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Subject, map, switchMap, takeUntil } from 'rxjs';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
+import { TodoCardComponent } from './todo-card.component';
 
 @Component({
   selector: 'app-todo-profile',
   standalone: true,
-  imports: [],
+  imports: [TodoCardComponent, MatCardModule],
   templateUrl: './todo-profile.component.html',
   styleUrl: './todo-profile.component.scss'
 })
-export class TodoProfileComponent {
+export class TodoProfileComponent implements OnInit, OnDestroy{
 
+  todo: Todo;
+  error: { help: string, httpResponse: string, message: string }
+
+  private ngUnsubscribe = new Subject<void>();
+
+  constructor(
+    private route: ActivatedRoute,
+    private todoService: TodoService) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      map((paramMap: ParamMap) => paramMap.get('id')),
+      switchMap((id: string) => this.todoService.getTodoById(id)),
+      takeUntil(this.ngUnsubscribe)
+    ).subscribe({
+      next: todo => this.todo = todo,
+      error: _err => {
+        this.error = {
+          help: 'Error fetching todo',
+          httpResponse: _err.message,
+          message: _err.error?.title,
+        };
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
 }
