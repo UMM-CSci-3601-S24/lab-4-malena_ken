0bbd82d2e733915751c730f0b344f5163924cc6f
Ken <brod0268@morris.umn.edu>
2024-02-13 20:38:40 -0600

    tried to copy user file layout to todo files
    only filtering owner from server

--- client/src/app/todos/todo.service.ts
+++ client/src/app/todos/todo.service.ts
@@ -8,27 +8,33 @@ import { Todo } from './todo';
  * Service that provides the interface for getting information
  * about `Todos` from the server.
  */
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class TodoService {
   // The URL for the todos part of the server API.
-  readonly todoUrl: string = environment.apiUrl + 'todos';
+  readonly todoUrl: string = `${environment.apiUrl}todos`;
+
+  private readonly ownerKey = 'owner';
 
   constructor(private httpClient: HttpClient) {
   }
 
-  getTodos(filters?: { category?: string }): Observable<Todo[]> {
+  getTodos(filters?: { owner?: string }): Observable<Todo[]> {
     // `HttpParams` is essentially just a map used to hold key-value
     // pairs that are then encoded as "?key1=value1&key2=value2&…" in
     // the URL when we make the call to `.get()` below.
     let httpParams: HttpParams = new HttpParams();
     if (filters) {
-      if (filters.category) {
-        httpParams = httpParams.set('category', filters.category)
+      if (filters.owner) {
+        httpParams = httpParams.set(this.ownerKey, filters.owner)
       }
     }
     // Send the HTTP GET request with the given URL and parameters.
     // That will return the desired `Observable<User[]>`
-    return this.httpClient.get<Todo[]>(this.todoUrl, { params: httpParams },);
+    return this.httpClient.get<Todo[]>(this.todoUrl, {
+      params: httpParams,
+    });
   }
 
   /**
@@ -38,7 +44,7 @@ export class TodoService {
    * @returns an `Observable` containing the resulting todo.
    */
   getTodoById(id: string): Observable<Todo> {
-    return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
+    return this.httpClient.get<Todo>(`${this.todoUrl}/${id}`);
   }
   /**
 * A service method that filters an array of `User` using
@@ -49,48 +55,20 @@ export class TodoService {
 * partial matches instead of waiting until we have a full string
 * to match against.
 *
-* @param users the array of `Users` that we're filtering
+* @param todos the array of `Users` that we're filtering
 * @param filters the map of key-value pairs used for the filtering
 * @returns an array of `Users` matching the given filters
 */
 
-  filterTodos(todos: Todo[], filters: { body?: string, owner?: string, status?: boolean, limit?: number }): Todo[] {
+  filterTodos(todos: Todo[], filters: { owner?: string}): Todo[] {
     let filteredTodos = todos;
 
-    if (filters.body) {
-      filters.body = filters.body.toLowerCase();
-      filteredTodos = filteredTodos.filter(todo => todo.body.toLowerCase().indexOf(filters.body) !== -1);
-    }
+    // Filter by owner
     if (filters.owner) {
       filters.owner = filters.owner.toLowerCase();
       filteredTodos = filteredTodos.filter(todo => todo.owner.toLowerCase().indexOf(filters.owner) !== -1);
     }
-    if (filters.status !== undefined) {
-      filteredTodos = filteredTodos.filter(todo => todo.status === filters.status);
-    }
-    if (filters.limit) {
-      filteredTodos = filteredTodos.slice(0, filters.limit);
-    }
+
     return filteredTodos;
   }
-
-
-/*   sortTodos(todos: Todo[], sortBy?: SortBy) {
-    const sortedTodos = todos;
-
-    if(sortBy === 'owner') {
-      sortedTodos.sort((todo1, todo2) => todo1.owner.localeCompare(todo2.owner))
-    }
-    else if(sortBy === 'category'){
-      sortedTodos.sort((todo1, todo2) => todo1.category.localeCompare(todo2.category))
-    }
-    else if(sortBy === 'body'){
-      sortedTodos.sort((todo1, todo2) => todo1.body.localeCompare(todo2.body))
-    }
-    else if(sortBy === 'status'){
-      sortedTodos.sort((todo1, todo2) => todo1.status.toString().localeCompare(todo2.status.toString()))
-    }
-
-    return sortedTodos;
-  }*/
 }
