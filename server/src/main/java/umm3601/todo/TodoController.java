d4edaf324df45f1c3882d297e5f51d0a67721a3b
mahon479 <mahon479@morris.umn.edu>
2024-02-17 13:09:39 -0600

    Updated getTodos() method.

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,19 +1,29 @@
 package umm3601.todo;
 
 
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.regex;
+import static com.mongodb.client.model.Filters.eq;
+
+import java.util.regex.Pattern;
+
 import java.util.ArrayList;
 
-
+import org.bson.Document;
 import org.bson.UuidRepresentation;
+import org.bson.conversions.Bson;
 import org.mongojack.JacksonMongoCollection;
 
 
 import com.mongodb.client.MongoDatabase;
-
+import com.mongodb.client.model.Sorts;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
+import java.util.List;
+import java.util.Objects;
+
 
 
 public class TodoController {
@@ -38,22 +48,55 @@ public class TodoController {
 
 
   public void getTodos(Context ctx) {
-    //int limit = 0;
-    //if (ctx.queryParamMap().containsKey("limit")) {
-      //limit = ctx.queryParamAsClass("limit", Integer.class)
-        //.check(it -> it >= 0, "Limit size should be non-negative")
-        //.get();
-    //}
+    Bson combinedFilter = constructFilter(ctx);
+    Bson sortingOrder = constructSortingOrder(ctx);
 
+    ArrayList<Todo> matchingTodos = todoCollection
+      .find(combinedFilter)
+      .sort(sortingOrder)
+      .into(new ArrayList<>());
 
-    ArrayList<Todo> todos = todoCollection
-        .find()
-        //.limit(limit)
-        .into(new ArrayList<>());
+    ctx.json(matchingTodos);
 
-
-    ctx.json(todos);
+    // Explicitly set the context status to OK
     ctx.status(HttpStatus.OK);
+  }
+
+  /**
+   *
+   * @param ctx a Javalin HTTP context, which contains the query parameters
+   *    used to construct the filter
+   * @return a Bson filter document that can be used in the `find` method
+   *   to filter the database collection of todos
+   */
+  private Bson constructFilter(Context ctx) {
+    List<Bson> filters = new ArrayList<>();
+
+    if (ctx.queryParamMap().containsKey("owner")) {
+      Pattern pattern = Pattern.compile(Pattern.quote(ctx.queryParam("owner")), Pattern.CASE_INSENSITIVE);
+      filters.add(regex("owner", pattern));
+    }
+
+
+    // Combine the list of filters into a single filtering document.
+    Bson combinedFilter = filters.isEmpty() ? new Document() : and(filters);
+
+    return combinedFilter;
+  }
+
+  /**
+   *
+   * @param ctx a Javalin HTTP context, which contains the query parameters
+   *   used to construct the sorting order
+   * @return a Bson sorting document that can be used in the `sort` method
+   *  to sort the database collection of todos
+   */
+  private Bson constructSortingOrder(Context ctx) {
+
+    String sortBy = Objects.requireNonNullElse(ctx.queryParam("sortby"), "name");
+    String sortOrder = Objects.requireNonNullElse(ctx.queryParam("sortorder"), "asc");
+    Bson sortingOrder = sortOrder.equals("desc") ?  Sorts.descending(sortBy) : Sorts.ascending(sortBy);
+    return sortingOrder;
   }
 }
 
