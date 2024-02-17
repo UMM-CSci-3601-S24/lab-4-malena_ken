f7945c22c72a8e76983ac1f1615a9a69cd46dc49
mahon479 <mahon479@morris.umn.edu>
2024-02-17 12:32:42 -0600

    Added todo controller and todo controller spec

---
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -0,0 +1,59 @@
+package umm3601.todo;
+
+
+import java.util.ArrayList;
+
+
+import org.bson.UuidRepresentation;
+import org.mongojack.JacksonMongoCollection;
+
+
+import com.mongodb.client.MongoDatabase;
+
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+
+
+public class TodoController {
+  private static final String API_TODOS = "/api/todos";
+  private final JacksonMongoCollection<Todo> todoCollection;
+
+
+  public TodoController(MongoDatabase db) {
+    todoCollection = JacksonMongoCollection
+    .builder()
+    .build(
+      db, "todos", Todo.class, UuidRepresentation.STANDARD
+    );
+  }
+
+
+
+
+  public void addRoutes(Javalin server) {
+    server.get(API_TODOS, this::getTodos);
+  }
+
+
+  public void getTodos(Context ctx) {
+    int limit = 0;
+    if (ctx.queryParamMap().containsKey("limit")) {
+      limit = ctx.queryParamAsClass("limit", Integer.class)
+        .check(it -> it >= 0, "Limit size should be non-negative")
+        .get();
+    }
+
+
+    ArrayList<Todo> todos = todoCollection
+        .find()
+        .limit(limit)
+        .into(new ArrayList<>());
+
+
+    ctx.json(todos);
+    ctx.status(HttpStatus.OK);
+  }
+}
+
