98e25c1b38de799e45453cb927191fda5c3f4b20
Ken Broden <brod0268@morris.umn.edu>
2024-02-18 16:31:57 -0600

    Added tests for addNewTodo()
    Co-authored-by: Malena Mahoney <mahon479 @morris.umn.edu>

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -7,8 +7,10 @@ import static com.mongodb.client.model.Filters.eq;
 
 import java.util.regex.Pattern;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.bson.Document;
 import org.bson.UuidRepresentation;
@@ -34,6 +36,8 @@ public class TodoController implements Controller {
 
   static final String OWNER_KEY = "owner";
   static final String SORT_ORDER_KEY = "sortorder";
+  static final int MAX_BODY_LENGTH = 200;
+  private static final String CATEGORY_REGEX = "^(software design|groceries|video games|homework)$";
 
 
   public TodoController(MongoDatabase db) {
@@ -118,9 +122,53 @@ public class TodoController implements Controller {
     return sortingOrder;
   }
 
+    /**
+   * Add a new todo using information from the context
+   * (as long as the information gives "legal" values to Todo fields)
+   *
+   * @param ctx a Javalin HTTP context that provides the todo info
+   *  in the JSON body of the request
+   */
+  public void addNewTodo(Context ctx) {
+
+    /*
+     * The follow chain of statements uses the Javalin validator system
+     * to verify that instance of `Todo` provided in this context is
+     * a "legal" todo. It checks the following things (in order):
+     *    - The todo has a value for the owner (`tdo.owner != null`)
+     *    - The todo owner is not blank (`tdo.owner.length > 0`)
+     *    - The provided status is strictly true or false
+     *    - The provided body is not blank (`tdo.body.length > 0`)
+     *    - The provided body is not too long (`tdo.body.length < MAX_BODY_LENGTH`)
+     *    - The provided category is one of "homework", "software design", "video games", or "groceries"
+     * If any of these checks fail, the validator will return a
+     * `BadRequestResponse` with an appropriate error message.
+     */
+    Todo newTodo = ctx.bodyValidator(Todo.class)
+    .check(tdo -> tdo.owner != null && tdo.owner.length() > 0, "Todo must have a non-empty owner")
+    .check(tdo -> tdo.status == true || tdo.status == false, "Status must be either true or false")
+    .check(tdo -> tdo.body != null && tdo.body.length() > 0 && tdo.body.length() < MAX_BODY_LENGTH, "Body must not be empty and less than " + MAX_BODY_LENGTH)
+    .check(tdo -> tdo.category != null && CATEGORY_REGEX.contains(tdo.category), "Category must be one of " + CATEGORY_REGEX)
+    .get();
+
+    // Insert the new todo into the database
+    todoCollection.insertOne(newTodo);
+
+    // Set the JSON response to be the `_id` of the newly created todo.
+    // This gives the client the opportunity to know the ID of the new todo,
+    // which it can use to perform further operations (e.g., display the todo).
+    ctx.json(Map.of("id", newTodo._id));
+    // 201 (`HttpStatus.CREATED`) is the HTTP code for when we successfully
+    // create a new resource (a todo in this case).
+    // See, e.g., https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    // for a description of the various response codes.
+    ctx.status(HttpStatus.CREATED);
+  }
+
   public void addRoutes(Javalin server) {
     server.get(API_TODO_BY_ID, this::getTodo);
     server.get(API_TODOS, this::getTodos);
+    server.post(API_TODOS, this::addNewTodo);
   }
 
 }
